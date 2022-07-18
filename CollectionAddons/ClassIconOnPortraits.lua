896468bfda1c0aa3cf4366d5dfb470d24839a156
Friskes <friskestv@gmail.com>
2022-07-18 23:11:19 +0300

    fix icon

--- CollectionAddons/ClassIconOnPortraits.lua
+++ CollectionAddons/ClassIconOnPortraits.lua
@@ -6,8 +6,19 @@ hooksecurefunc("UnitFramePortrait_Update", function(self)
             local t = CLASS_ICON_TCOORDS[select(2, UnitClass(self.unit))]
             if t then
                 self.portrait:SetTexture("Interface\\TargetingFrame\\UI-Classes-Circles")
-                --self.portrait:SetTexture("Interface\\WorldStateFrame\\Icons-Classes")
+                -- self.portrait:SetTexture("Interface\\WorldStateFrame\\Icons-Classes")
                 self.portrait:SetTexCoord(unpack(t))
+
+                if self.unit == 'targettarget' then
+                    self.portrait:ClearAllPoints()
+                    self.portrait:SetPoint("LEFT", TargetFrameToT, "LEFT", 3.5, -0.5)
+                    self.portrait:SetSize(TargetFrameToT:GetHeight() - 6.3, TargetFrameToT:GetHeight() - 6.3)
+                end
+                if self.unit == 'focus-target' then
+                    self.portrait:ClearAllPoints()
+                    self.portrait:SetPoint("LEFT", FocusFrameToT, "LEFT", 3.5, -0.5)
+                    self.portrait:SetSize(FocusFrameToT:GetHeight() - 6.3, FocusFrameToT:GetHeight() - 6.3)
+                end
             end
         else
             self.portrait:SetTexCoord(0, 1, 0, 1)
